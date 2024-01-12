9dd675a76cec3956d3573cd64e59eaebb323450e
dalssak <81132564+j1sung@users.noreply.github.com>
2024-01-13 02:56:50 +0900

    Time: 36 ms (67.24%), Space: 17.4 MB (14.67%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,10 +1,32 @@
+"""
+author : Ahn Ji Sung
+github : https://github.com/j1sung
+e-mail : dalssagi00@gmail.com
+title : two-sum
+description : Binary
+"""
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        output = a+b
-        return output
-
-'''
-덧셈 연산(음수, 양수)
-
-'''
+        if a==0 and b==0:
+            return 0
+        if a==0 and b!=0:
+            return b
+        if a!=0 and b==0:
+            return a
+        if a<0 and b<0:
+            for i in range(abs(b)):
+                a-=1
+            return a
+        elif a>0 and b>0:
+            for i in range(b):
+                a+=1
+            return a
+        elif a<0 and b>0:
+            for i in range(b):
+                a+=1
+            return a    
+        elif a>0 and b<0:
+            for i in range(a):
+                b+=1
+            return b
         
