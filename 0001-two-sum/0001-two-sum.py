3173ced3858fa69a2730648ebacdc85a2de3dd54
dalssak <81132564+j1sung@users.noreply.github.com>
2024-01-13 01:19:13 +0900

    Time: 1908 ms (28.22%), Space: 18.1 MB (31.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]: #함수의 매개변수에서 '콜론:'은 변수 타입에 대한 주석이며 코드 강제성은 없다
         output = []
-        for i in range(len(nums)):
+        for i in range(len(nums)): # 2, 7, 11, 15
             i_val=nums[i]
-            for j in range(len(nums)):
+            for j in range(len(nums)): # 2, 7, 11, 15
                 if i==j:
-                    continue
+                    break
                 j_val=nums[j]
                 if i_val+j_val == target:
                     output.append(i)
                     output.append(j)
                     return output
                 
+2,7/2,11/ 2,15
+7,11/7,15
+11,15
 
 
 
